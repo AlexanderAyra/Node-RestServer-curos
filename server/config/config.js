44c072b2e1f05ecaf9c195820b7e886a44dccafa
Alexander <alexander.ayras@gmail.com>
2020-05-04 01:34:19 -0500

    actualizacion mongodb

--- server/config/config.js
+++ server/config/config.js
@@ -16,11 +16,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
 
-// if (process.env.NODE_ENV === 'dev') {
+if (process.env.NODE_ENV === 'dev') {
 
-//     urlDB = 'mongodb://localhost:27017/cafe';
-// } else {
-urlDB = 'mongodb+srv://strider:NT7J8gtXnPtxikVc@cluster0-8unoh.mongodb.net/cafe'
-    //}
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://strider:NT7J8gtXnPtxikVc@cluster0-8unoh.mongodb.net/cafe'
+}
 
 process.env.URLDB = urlDB;
